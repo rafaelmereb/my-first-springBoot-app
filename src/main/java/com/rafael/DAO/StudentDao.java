137bd7cf4919235beaa300f50e01ceab0d7da484
Rafael <rafaelcdmm@gmail.com>
2017-04-01 20:50:27 -0300

    Adicionando projeto local
    Adicionando projeto local

---
+++ src/main/java/com/rafael/DAO/StudentDao.java
@@ -0,0 +1,17 @@
+package com.rafael.DAO;
+
+import com.rafael.Entity.Student;
+
+import java.util.Collection;
+
+public interface StudentDao {
+    Collection<Student> getAllStudents();
+
+    Student getStudentById(int id);
+
+    void removeStudentById(int id);
+
+    void updateStudent(Student student);
+
+    void insertStudent(Student student);
+}
---
+++ src/main/java/com/rafael/Service/StudentService.java
@@ -0,0 +1,37 @@
+package com.rafael.Service;
+
+import com.rafael.DAO.StudentDao;
+import com.rafael.Entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    @Qualifier("mongoData")
+    private StudentDao studentDao;
+
+    public Collection<Student> getAllStudents(){
+        return this.studentDao.getAllStudents();
+    }
+
+    public Student getStudentById(int id){
+        return this.studentDao.getStudentById(id);
+    }
+
+    public void removeStudentById(int id) {
+        this.studentDao.removeStudentById(id);
+    }
+
+    public void updateStudent(Student student){
+        this.studentDao.updateStudent(student);
+    }
+
+    public void insertStudent(Student student) {
+        this.studentDao.insertStudent(student);
+    }
+}
